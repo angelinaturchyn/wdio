a82a3c211fae533389fb81ff69c0bf1ad10860ef
angelinaturchyn <75140309+angelinaturchyn@users.noreply.github.com>
2022-01-15 22:29:21 -0600

    Create auth.spec.js

---
+++ specs/auth.spec.js
@@ -0,0 +1,38 @@
+import LoginPage from '../pages/login.page';
+import  ProfilePage from '../pages/profile.page'
+
+describe('Auth',function(){
+    beforeEach(async function(){
+        await LoginPage.open();
+    });
+
+    it('Successful log in', async function () {
+        //await LoginPage.open();
+        await expect(LoginPage.buttonSubmit)
+            .toBeDisabled();
+        await LoginPage.login(process.env.LOGIN, process.env.PASSWORD);
+        await expect(ProfilePage.iconUser)
+            .toBeDisplayed();
+    });
+
+    it('Log-in attempt with non-registered username/password',async function() {
+        await LoginPage.login('invalid@gmail.com', 'invalid');
+        await expect(LoginPage.notification).toHaveText('Email is not registered');
+    });
+
+    it('Log-in attempt with invalid password',async function() {
+        await LoginPage.login(process.env.LOGIN, 'invalid');
+        await expect(LoginPage.notification).toHaveText('Incorrect password');
+    });
+
+    it('Credentials are required', async function (){
+        await LoginPage.inputUsername.setValue('test');
+        await LoginPage.inputUsername.smartClear();
+        await expect(LoginPage.loginError).toHaveText('Required');
+        await LoginPage.inputPassword.setValue('test');
+        await LoginPage.inputPassword.smartClear();
+        await expect(LoginPage.passwordError).toHaveText('Required');
+
+    });
+
+});
